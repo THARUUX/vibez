0861b4a1a34081e3c983c3b63d723a5e6e680ab9
THARUUX <tdamsara6@gmail.com>
2026-04-23 11:16:28 +0530

    apple bug

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -32,9 +32,9 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
                     {/* <Navigation /> */}
 
                     <CartDrawer />
-                    {/* <main className="pt-20 min-h-screen">
+                    <main className="pt-20 min-h-screen">
                         {children}
-                    </main> */}
+                    </main>
                     <Footer />
                 </>
             )}
